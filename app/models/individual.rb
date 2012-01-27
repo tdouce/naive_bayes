4a16cb47b9bc73947d09f3b92c0eea031cb1e32d
Travis Douce <travisdouce@gmail.com>
2012-01-26 21:31:38 -0500

    Organizes models.  Adds scope in individual#rb. Adds constants for male and female probabilities.  Utilizes scope in sample#rb and prepares sample data to use module

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -1,11 +1,25 @@
 class Individual < ActiveRecord::Base
 
-  MALE   = 'male'
-  FEMALE = 'female'
-
   validates :height, :presence => true, :numericality => true
   validates :weight, :presence => true, :numericality => true
   validates :gender, :presence => true
 
+  MALE    = 'Male'
+  FEMALE  = 'Female'
+
+  # Is the dynamic scope below better?
+  #scope :males,   Individual.where(:gender => Individual::MALE)
+  #scope :females, Individual.where(:gender => Individual::FEMALE)
+
+  # Get all individuals according to a specific gender
+  def self.get_gender(gender)
+    Individual.where(:gender => gender )
+  end
+
+  MALEPROB   = 0.5
+  FEMALEPROB = 0.5
+
   GENDERS = [MALE, FEMALE]
+
+
 end
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,11 +1,36 @@
 class Sample < ActiveRecord::Base
 
+  #attr_accessible :test
+
+  # Determine gender of a sample by using the Naive Bayes Classifier
+  include NaiveBayes
+
   validates :weight,    :presence => true, :numericality => true
   validates :height,    :presence => true, :numericality => true
   validates :foot_size, :presence => true, :numericality => true
 
-  # Remove
   def test
+
+    #males   = Individual.males.map {|male| [ male.weight, male.height, male.foot_size ]}.transpose
+    #females = Individual.males.map {|male| [ male.weight, male.height, male.foot_size ]}.transpose
+
+    males   = Individual.get_gender(Individual::MALE).map {|male| [ male.weight, male.height, male.foot_size ]}.transpose
+    females = Individual.get_gender(Individual::FEMALE).map {|female| [ female.weight, female.height, female.foot_size ]}.transpose
+
+    training_data = {}
+
+    # See if there is a way to dynamically build this list based on the number of items in the
+    # form or possibly the number of columns (minus id, created_at, etc) that are in the database table
+    training_data[ Individual::MALE ]   = males 
+    training_data[ Individual::FEMALE ] = females 
+
+    #a = GenderDecider.new()
+    #a.train( training_data )
+    #a.get_posteriors( sample, Individual::MALEPROB, Individual::FEMALEPROB )
+    #result = a.classify( Individual::MALE, Individual::FEMALE )
+
+    # Testing response for AJAX
     'Yes, it is!'
+
   end
 end
