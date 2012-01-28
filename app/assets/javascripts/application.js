8b19cea5e30489950b2fc7606ed86fdfe7c4a51e
Travis Douce <travisdouce@gmail.com>
2012-01-28 12:08:11 -0500

    Adds javascript to check for extreme values on form submission. If there are 'extreme' values then hilite fields and presnet confirmbox. Changes css class. Removes test code in _result#html#erb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,31 +8,64 @@
 //= require jquery_ujs
 //= require underscore-min.js
 //= require_tree .
+
 $(function(){
 
+   // ids and reasonable minumum and maxium values.  These will be used to check
+   // the input values on form submission to make sure that a user did not
+   // accidently type in an 'extreme' value.
    var check_ids = [
-                    [ 'sample_weight',   50, 300 ],
-                    [ 'sample_height',   4,  7 ],
-                    ['sample_foot_size', 4,  14 ]
+                    [ 'sample_weight',    50, 300 ],
+                    [ 'sample_height',    4,  7 ],
+                    [ 'sample_foot_size', 4,  14 ]
                    ]
 
-   _.each([1, 2, 3], function(num){ alert(num); });
-
-   // for(var i in arr) {
-   //   var value = arr[i];
-   //   alert(i =") "+ value);
-   // }
-
-  $('#new_sample').submit(function(){
-    if ( $('#sample_weight').val() > 400 || $('#sample_height').val() > 7 || $('#sample_foot_size').val() > 15 )
-      {
-        var confirm_popup = confirm("Some of numbers are really large? 'Cancel' to edit your numbers and 'OK' to continue with submission.")
-        if ( confirm_popup != true )
-          return false
-      };
+  // On click make the value for each input '', remove any previous result, and
+  // remove any classes they may exist from previous submissions
+  $("#clear_button").click(function(){
+    _.each( check_ids, function( id ){ 
+        $('#' + id).val(''); 
+    });
+    $('#response').html('<p></p>');
+    $('*').removeClass('extreme_number');
   });
 
+  // On form submission check to see if any of the input values are extreme
+  // using the ids and values from 'check_ids'.  If there are extreme values
+  // then highlit those input fields and prompt a confirm box.
+  $('#submit_sample').click(function(event){
 
+    // Remove any previous result
+    $('#response').html('<p></p>');
 
+    // Remove any previous warning
+    $('*').removeClass('extreme_number');
 
+    // Value used to see if the confirm alert should be presented
+    var warning = false;
+
+    // Loop over each input and check if input value is below min or above max
+    _.each( check_ids, function( id ){ 
+      if ( $('#' + id[0]).val() < id[1] || $('#' + id[0]).val() > id[2]  )
+          {
+            // Set warning to true
+            warning = true;
+            // Add warning class
+            $('#' + id[0]).addClass('extreme_number');
+          };
+    });
+      
+    // If any of the input values were 'extreme', then present a confirm box
+    if ( warning == true ){
+      var confirm_popup = confirm("Some numbers seem extreme. Are you sure they are correct? 'Cancel' to edit your numbers and 'OK' to continue with submission.");
+
+      // If the user wants to edit the values then stop form submission
+      if ( confirm_popup == false )
+        { event.preventDefault(); }
+      // If user does NOT want to edit form values then continue with submission
+      // and remove any warnings
+      else 
+        { $('*').removeClass('extreme_number'); }
+    };
+  });
  });
