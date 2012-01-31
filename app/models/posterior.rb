598310594adcee2791d77729f8ecbb788b491f37
Travis Douce <travisdouce@gmail.com>
2012-01-30 20:18:09 -0500

    Add comments

--- app/models/posterior.rb
+++ app/models/posterior.rb
@@ -1,6 +1,9 @@
+
+# Stores the previous requests' means and variances per gender
 class Posterior < ActiveRecord::Base
   serialize :stats
 
+  # Only keep on record of means_variances in the table at a time.
   def self.posterior_for_next_request( means_variances )
     if Posterior.count == 0
       Posterior.create( :stats => means_variances )
