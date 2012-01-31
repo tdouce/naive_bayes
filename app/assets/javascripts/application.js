2611fa87c148d23c52d198224bc8e41fda4a7bb8
Travis Douce <travisdouce@gmail.com>
2012-01-30 20:42:22 -0500

    Adds comments. Re-uses function to clear warnings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,85 +11,94 @@
 
 $(function(){
 
-  var weight_min    = 50;
-  var weight_max    = 300;
-  var height_min    = 4;
-  var height_max    = 7;
-  var foot_size_min = 4;
-  var foot_size_max = 14;
+  // Provides warnings for forms if user inputs values that are unusually large or small.
+  function warnings(){
 
-   // ids and reasonable minumum and maxium values.  These will be used to check
-   // the input values on form submission to make sure that a user did not
-   // accidently type in an 'extreme' value.
-   var check_ids = [
-                    [ 'sample_weight',        weight_min, weight_max],
-                    [ 'sample_height',        height_min,  height_max ],
-                    [ 'sample_foot_size',     foot_size_min,  foot_size_max  ],
-                    [ 'individual_weight',    weight_min, weight_max ],
-                    [ 'individual_height',    height_min,  height_max ],
-                    [ 'individual_foot_size', foot_size_min,  foot_size_max  ]
-                   ];
+    // Maximum and minimum weights used to generate warnings
+    var weight_min    = 50;
+    var weight_max    = 300;
+    var height_min    = 4;
+    var height_max    = 7;
+    var foot_size_min = 4;
+    var foot_size_max = 14;
 
-  function remove_warnings(){
-  
-    // Remove any errors that might exist
-    $('.error_explination').remove();
+     // Used to check the input values on form submission to make sure that a user did not
+     // accidently type in an 'extreme' value. [ input id, minimum value, maximum value ]
+     var check_ids = [
+                      [ 'sample_weight',        weight_min, weight_max],
+                      [ 'sample_height',        height_min,  height_max ],
+                      [ 'sample_foot_size',     foot_size_min,  foot_size_max  ],
+                      [ 'individual_weight',    weight_min, weight_max ],
+                      [ 'individual_height',    height_min,  height_max ],
+                      [ 'individual_foot_size', foot_size_min,  foot_size_max  ]
+                     ];
 
-    // Clearn any previous ajax response
-    $('#response').html('<p></p>');
+    // Remove any previous warnings that may exist
+    function remove_warnings(){
+    
+      // Remove any validation errors that might exist
+      $('.error_explination').remove();
 
-    // Clear any previous extreme number warnings
-    $('*').removeClass('extreme_number');
-  };
+      // Clearn any previous ajax response
+      $('#response').html('<p></p>');
 
-  // On click make the value for each input '', remove any previous result, and
-  // remove any classes they may exist from previous submissions
-  $("#clear_button").click(function(){
-
-    // Remove any previous validations or warnings
-    remove_warnings();
-
-    // Clear all values
-    _.each( check_ids, function( id ){ $('#' + id).val(''); });
-
-  });
-
-  // On form submission check to see if any of the input values are extreme
-  // using the ids and values from 'check_ids'.  If there are extreme values
-  // then highlit those input fields and prompt a confirm box.
-  $('#submit_sample').click(function(event){
-
-    // Remove any previous validations or warnings
-    remove_warnings();
-
-    // Value used to see if the confirm alert should be presented
-    var warning = false;
-
-    // Loop over each input and check if input value is below min or above max
-    _.each( check_ids, function( id ){ 
-      if ( $('#' + id[0]).val() < id[1] || $('#' + id[0]).val() > id[2]  ) {
-
-            // Set warning to true
-            warning = true;
-
-            // Add warning class
-            $('#' + id[0]).addClass('extreme_number');
-          };
-    });
-      
-    // If any of the input values were 'extreme', then present a confirm box
-    if ( warning == true ){
-
-      var confirm_popup = confirm("Are you sure the values are correct? 'Cancel' to edit and 'OK' to continue with submission.");
-
-      // If the user wants to edit the values then stop form submission
-      if ( confirm_popup == false )
-        { event.preventDefault(); }
-
-      // If user does NOT want to edit form values then continue with submission
-      // and remove any warnings
-      else 
-        { $('*').removeClass('extreme_number'); }
+      // Clear any previous extreme number warnings
+      $('*').removeClass('extreme_number');
     };
-  });
+
+    // On click clear each input value, remove any previous result, and
+    // remove any classes they may exist from previous submissions
+    $("#clear_button").click(function(){
+
+      // Remove any previous validations or warnings
+      remove_warnings();
+
+      // Clear all values
+      _.each( check_ids, function( id ){ $('#' + id).val(''); });
+
+    });
+
+    // On form submission check to see if any of the input values are extreme
+    // using the ids and values from 'var check_ids'.  If there are extreme values
+    // then highlit those input fields and prompt a confirm box.
+    $('#submit_sample').click(function(event){
+
+      // Remove any previous validations or warnings
+      remove_warnings();
+
+      // Value used to see if the confirm alert should be presented
+      var warning = false;
+
+      // Loop over each input and check if input value is below min or above max
+      _.each( check_ids, function( id ){ 
+        if ( $('#' + id[0]).val() < id[1] || $('#' + id[0]).val() > id[2]  ) {
+
+              // Set warning to true
+              warning = true;
+
+              // Add warning class
+              $('#' + id[0]).addClass('extreme_number');
+            };
+      });
+        
+      // If any of the input values were 'extreme', then present a confirm box
+      if ( warning == true ){
+
+        var confirm_popup = confirm("Are you sure the values are correct? 'Cancel' to edit and 'OK' to continue with submission.");
+
+        // If the user wants to edit the values then stop form submission
+        if ( confirm_popup == false )
+          { event.preventDefault(); }
+
+        // If user does NOT want to edit form values then continue with submission and remove any warnings
+        else 
+          // Remove any previous validations or warnings
+          { remove_warnings(); };
+      };
+     });
+    };
+
+  // Invoke warnings 
+  warnings()
+
  });
