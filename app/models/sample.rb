00cb8f319ad66d1ca7c2183fe8ec4de9deb75e08
Travis Douce <travisdouce@gmail.com>
2012-01-30 20:11:27 -0500

    Puts naive bayes classier code in module.  This module is included in sample#rb and is used to calculate the naive bayes classier algorithm.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,6 +1,4 @@
 
-#require 'naive_bayes_classifier'
-
 class Sample < ActiveRecord::Base
 
   include NaiveBayesClassifier
@@ -10,13 +8,14 @@ class Sample < ActiveRecord::Base
   validates :foot_size, :presence => true, :numericality => true
 
   # Attributes that we are included in naive bayes classier
+  # If you want to add another attribute to be included in the
+  # NaiveBayesClassier, then add it here.
   FORMATTRIBUTES = [ :weight, :height, :foot_size ]
 
   # Packages up data for a sumbited sample
   def prepare_sample
     sample_result = FORMATTRIBUTES.inject([]) { |result,attr| result << self.send( attr ) }
   end
-
 
   # If there is no training data then do not run any calculation and instead return 
   # text notifing user. If there is data and at least one individual has :trained => false, 
@@ -27,11 +26,6 @@ class Sample < ActiveRecord::Base
   # for new and is set to false if an individual was edited ), then retreive
   # means and variances from previous request.
   def run_sample( sample_data )
-
-    #a = testing
-
-    #debugger
-    
 
     # Run sample only if there is training data
     if Individual.all.count != 0 
@@ -48,10 +42,10 @@ class Sample < ActiveRecord::Base
         means_variances = train( prepped_data )
 
         # Generate the posterior numerators per gender
-        male_posterior_result, female_posterior_result = posteriors( sample_data, Individual::MALEPROB, Individual::FEMALEPROB, means_variances )
+        male_posterior, female_posterior = posteriors( sample_data, Individual::MALEPROB, Individual::FEMALEPROB, means_variances )
 
         # Determine classification
-        result = classify( Individual::MALE, Individual::FEMALE, male_posterior_result, female_posterior_result )
+        result = classify( Individual::MALE, Individual::FEMALE, male_posterior, female_posterior )
 
         # Set trained status to true for every individual that previously had a trained status as false
         untrained_individuals.update_all( :trained => true )
@@ -72,10 +66,10 @@ class Sample < ActiveRecord::Base
         means_variances = previously_trained.stats 
 
         # Generate posterior numerators per gender
-        male_posterior_result, female_posterior_result = posteriors( sample_data, Individual::MALEPROB, Individual::FEMALEPROB, means_variances )
+        male_posterior, female_posterior = posteriors( sample_data, Individual::MALEPROB, Individual::FEMALEPROB, means_variances )
 
         # Determine classification
-        result = classify( Individual::MALE, Individual::FEMALE, male_posterior_result, female_posterior_result )
+        result = classify( Individual::MALE, Individual::FEMALE, male_posterior, female_posterior )
 
       end
 
@@ -86,10 +80,10 @@ class Sample < ActiveRecord::Base
   end
 
   private
-
-      # Querys database for each gender according to the attributes and packages it up in a hash so 
-      # that it can be sent to NaiveBayes classifer
-      def prepare_data( attributes )
+    
+    # Querys database for each gender according to FORMATTRIBUTES and packages it up in a hash so 
+    # that it can be sent to NaiveBayesClassifer
+    def prepare_data( attributes )
 
       training_data = Individual::GENDERS.inject({}) do |hash,gender| 
 
@@ -103,72 +97,5 @@ class Sample < ActiveRecord::Base
       end
 
       return training_data 
-      end
-
-      # Generates means and variances
-      def batch_train( data )
-          means = []
-          variances = []
-
-          data.each do |d|
-              d.map! {|num| Float( num ) }
-              mean = d.mean 
-              means << mean 
-              variances << d.variance( mean ) 
-          end
-          
-          return means, variances
-      end
-
-      # Train data, get posterior for male and female, and compare results to determine gender 
-      def train( training_data )
-
-          data = Individual::GENDERS.inject({}) do |hash,gender|
-            means, variances = batch_train( training_data[ gender ] )
-            hash[ gender ] = [ means, variances ]
-            hash
-          end
-
-          return data 
-      end
-
-      # Generate posterior
-      def posterior_for_gender( gender_probability, variances, means, sample )
-
-          # Make and array for all probabilities
-          probs = 1.upto( sample.size ).each_with_index.inject([]) do |probs, (attr, index)|
-              probs <<  probability( variances[ index ], means[ index ], sample[ index ] )
-              probs
-          end
-
-          # Add gender probability
-          probs << gender_probability
-          
-          puts 'probs: '
-          puts probs
-
-          # Generate product of array 
-          probs.inject(){|product, element| product * element}
-      end
-      
-      # Generate posterior per gender
-      def posteriors( sample, male_probability, female_probability, means_variances )
-          
-          male_posterior_numerator   = posterior_for_gender( male_probability, means_variances[Individual::MALE][1], means_variances[Individual::MALE][0], sample )
-          female_posterior_numerator = posterior_for_gender( female_probability, means_variances[Individual::FEMALE][1], means_variances[Individual::FEMALE][0], sample )
-
-          return male_posterior_numerator, female_posterior_numerator
-      end
-
-      # Generates probability for each combination (i.e. p(height|male), etc )
-      def probability( variance, mean, measurement )
-          male_numerator = ( 1 /( Math.sqrt( 2 * Math::PI * variance ) ))
-          male_exponent = ( (-( measurement - mean)**2 ) / ( 2 * variance) )
-          probability = male_numerator * Math.exp( male_exponent ) 
-      end
-
-      # Determine gender by comparing posteriors
-      def classify( male_text, female_text, male_posterior_numerator, female_posterior_numerator )
-          male_posterior_numerator > female_posterior_numerator ? male_text : female_text
-      end
+    end
 end
--- lib/naive_bayes_classifier.rb
+++ lib/naive_bayes_classifier.rb
@@ -1,44 +1,37 @@
 
+# Based on the Naive Bayes Classier equation found at http://en.wikipedia.org/wiki/Naive_Bayes_classifier
 module NaiveBayesClassifier 
 
-  def testing
-    'from module'
-  end
-
-  # Generates means and variances. Accepts nested arrays of data. 
-  # Returns an array of the means and an array of the variances
+  # Generates means and variances
   def batch_train( data )
-      means = []
-      variances = []
 
-      data.each do |d|
-          d.map! {|num| Float( num ) }
-          mean = d.mean 
-          means << mean 
-          variances << d.variance( mean ) 
-      end
-      
-      return means, variances
+    means = []
+    variances = []
+
+    data.each do |d|
+        d.map! {|num| Float( num ) }
+        mean = d.mean 
+        means << mean 
+        variances << d.variance( mean ) 
+    end
+    
+    return means, variances
   end
 
-  #Train data, get posterior for male and female, and compare results to determine gender 
+  # Train data, get posterior for male and female, and compare results to determine gender 
   def train( training_data )
 
-      # Get arrays containing means and variances
-      male_means, male_variances = batch_train( training_data[ Individual::MALE ] )
-
-      ## Get arrays containing means and variances
-      female_means, female_variances = batch_train( training_data[ Individual::FEMALE ] )
-
-      data = {}
-      data['male'] = [ male_means, male_variances ]
-      data['female'] = [ female_means, female_variances ]
+      data = Individual::GENDERS.inject({}) do |hash,gender|
+        means, variances = batch_train( training_data[ gender ] )
+        hash[ gender ] = [ means, variances ]
+        hash
+      end
 
       return data 
   end
 
-  # Generates posterior
-  def get_posterior_for_gender( gender_probability, variances, means, sample )
+  # Generate posterior
+  def posterior_for_gender( gender_probability, variances, means, sample )
 
       # Make and array for all probabilities
       probs = 1.upto( sample.size ).each_with_index.inject([]) do |probs, (attr, index)|
@@ -52,22 +45,15 @@ module NaiveBayesClassifier
       puts 'probs: '
       puts probs
 
-      # Multiply all elements in array together
+      # Generate product of array 
       probs.inject(){|product, element| product * element}
   end
   
-  # Generate posterior for male and female
-  def get_posteriors( sample, male_probability, female_probability, means_variances )
+  # Generate posterior per gender
+  def posteriors( sample, male_probability, female_probability, means_variances )
       
-      puts '*'*80
-      puts means_variances
-      puts '*'*80
-        
-      # Get posterior for male classification
-      male_posterior_numerator = get_posterior_for_gender( male_probability, means_variances['male'][1], means_variances['male'][0], sample )
-
-      # Get posterior for female classification
-      female_posterior_numerator = get_posterior_for_gender( female_probability, means_variances['female'][1], means_variances['female'][0], sample )
+      male_posterior_numerator   = posterior_for_gender( male_probability, means_variances[Individual::MALE][1], means_variances[Individual::MALE][0], sample )
+      female_posterior_numerator = posterior_for_gender( female_probability, means_variances[Individual::FEMALE][1], means_variances[Individual::FEMALE][0], sample )
 
       return male_posterior_numerator, female_posterior_numerator
   end
@@ -79,11 +65,9 @@ module NaiveBayesClassifier
       probability = male_numerator * Math.exp( male_exponent ) 
   end
 
-  # Determine if male and female by comparing posteriors
+  # Determine gender by comparing posteriors
   def classify( male_text, female_text, male_posterior_numerator, female_posterior_numerator )
       male_posterior_numerator > female_posterior_numerator ? male_text : female_text
   end
-
-
 end
 
